993aaed4f51907ebe83fc4c11bb8061d606b9ac1
yanyiguo <yanyiguo@foresee.com.cn>
2022-06-09 17:52:45 +0800

    提交example项目

---
+++ example/src/main/java/com/yanyiguo/github/example/HelloGit.java
@@ -0,0 +1,10 @@
+package com.yanyiguo.github.example;
+
+/**
+ * @author yyguo_826@163.com
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
